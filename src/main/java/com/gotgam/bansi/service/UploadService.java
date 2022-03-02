7ecb682dfdbc1a5fadb06e03d6fa3d9e2d1f799d
zzzinho <hahoh0013@gmail.com>
2022-03-02 16:27:05 +0900

    witout s3

--- src/main/java/com/gotgam/bansi/service/UploadService.java
+++
@@ -1,6 +0,0 @@
-package com.gotgam.bansi.service;
-
-// public interface UploadService {
-//     void uploadFile(InputStream inputStream, ObjectMetadata objectMetadata, String filename);
-//     String getFileUrl(String filename);
-// }
