71e71f38b38a72c5124d12d17035e44da98013dd
jinho jeong <hahoh0013@gmail.com>
2022-03-24 15:36:38 +0900

    add and delete folder content

--- src/main/java/com/gotgam/bansi/controller/ArchiveController.java
+++ src/main/java/com/gotgam/bansi/controller/ArchiveController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.gotgam.bansi.DTO.ArchiveFolderDTO.ArchiveFolderRequest;
 import com.gotgam.bansi.DTO.ArchiveFolderDTO.ArchiveFolderResponse;
 import com.gotgam.bansi.DTO.ArchiveFolderDTO.ListArchiveFolderResponse;
+import com.gotgam.bansi.DTO.ArchiveLinkDTO.ArchiveLinkRequest;
 import com.gotgam.bansi.DTO.ResponseDTO;
 import com.gotgam.bansi.model.ArchiveFolder;
 import com.gotgam.bansi.service.ArchiveFolderService;
@@ -20,6 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
+
+
 
 
 
@@ -63,6 +66,39 @@ public class ArchiveController {
         ArchiveFolder folder = folderService.getFolder(folderId); 
         return ResponseEntity.ok().body(new ArchiveFolderResponse("S00", "폴더 가져오기 성공", folder));
     }
-
     
+    @RequestMapping(value="/folder/{folderId}/piece/{pieceId}/", method=RequestMethod.POST)
+    public ResponseEntity<ArchiveFolderResponse> addPieceToFolder(@PathVariable Long folderId, @PathVariable Long pieceId) {
+        ArchiveFolder folder = folderService.addPieceToFolder(pieceId, folderId);
+        return ResponseEntity.ok().body(new ArchiveFolderResponse("S00", "아카이브 폴더에 조각 담기 성공", folder));
+    }
+
+    @RequestMapping(value="/folder/{folderId}/piece/{pieceId}/", method=RequestMethod.DELETE)
+    public ResponseEntity<ArchiveFolderResponse> removePieceFromFolder(@PathVariable Long folderId, @PathVariable Long pieceId) {
+        ArchiveFolder folder = folderService.deletePieceFromFolder(pieceId, folderId);
+        return ResponseEntity.ok().body(new ArchiveFolderResponse("S00", "아카이브 폴더에서 조각 삭제 성공", folder));
+    }
+    
+    @RequestMapping(value="/folder/{folderId}/collection/{collectionId}/", method=RequestMethod.POST)
+    public ResponseEntity<ArchiveFolderResponse> addCollectionToFolder(@PathVariable Long folderId, @PathVariable Long collectionId) {
+        ArchiveFolder folder = folderService.addCollectionToFolder(collectionId, folderId);
+        return ResponseEntity.ok().body(new ArchiveFolderResponse("S00", "아카이브 폴더에 조각 모음 담기 성공", folder));
+    }
+
+    @RequestMapping(value="/folder/{folderId}/collection/{collectionId}/", method=RequestMethod.DELETE)
+    public ResponseEntity<ArchiveFolderResponse> removeCollectionFromFolder(@PathVariable Long folderId, @PathVariable Long collectionId) {
+        ArchiveFolder folder = folderService.deleteCollectionFromFolder(collectionId, folderId);
+        return ResponseEntity.ok().body(new ArchiveFolderResponse("S00", "아카이브 폴더에서 조각 모음 삭제 성공", folder));
+    }
+    @RequestMapping(value="/folder/{folderId}/link/", method=RequestMethod.POST)
+    public ResponseEntity<ArchiveFolderResponse> addLinkToFolder(@PathVariable Long folderId, @RequestBody ArchiveLinkRequest linkDto) {
+        ArchiveFolder folder = folderService.addLinkToFolder(linkDto, folderId);
+        return ResponseEntity.ok().body(new ArchiveFolderResponse("S00", "아카이브 폴더에 링크 담기 성공", folder));
+    }
+
+    @RequestMapping(value="/folder/{folderId}/link/{linkId}/", method=RequestMethod.DELETE)
+    public ResponseEntity<ArchiveFolderResponse> removeLinkFromFolder(@PathVariable Long folderId, @PathVariable Long linkId) {
+        ArchiveFolder folder = folderService.deleteLinkFromFolder(linkId, folderId);
+        return ResponseEntity.ok().body(new ArchiveFolderResponse("S00", "아카이브 폴더에서 조각 삭제 성공", folder));
+    }
 }
